81fa843ca2a9e42e494f153d9a06766cb1d9cc76
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2023-10-17 09:12:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+name = "Pavan"
+names = "Pavan, Navdeep, Viraj, Saran"
+print(len(names))
+
+print(names[0])
+print(names[3:6])
+print(names[:3])
+print(names[3:])
+print(names[:-2])
+print(names[-3:28])
+
