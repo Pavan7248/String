ae0ca09d2bf661fada6f2ee39cf2c89e65620b9d
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2023-10-18 04:51:27 +0000

    Practiced String methods

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
+#Strings are immutable
 name = "Pavan"
 names = "Pavan, Navdeep, Viraj, Saran"
-print(len(names))
 
+print(len(names))
 print(names[0])
 print(names[3:6])
 print(names[:3])
 print(names[3:])
 print(names[:-2])
 print(names[-3:28])
+print(name.upper())
+print(name.lower())
+str1 =  "my favourite fruit is mango"
+print(str1.capitalize())
+
+str = "My name is Pavan"
+print(str.index("is"))
+
+str2 = "MyNameIsPreet3"
+print(str2.isalnum())
+
+print(str2.isalpha())
+print(len(str))
+
+
+
+
+
+
+
+
+
+
 
